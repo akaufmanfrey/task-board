98a4ff036cc7f525bf4c8360e2f70638e247105f
akaufmanfrey <akaufmanfrey@gmail.com>
2024-07-09 10:43:25 -0400

    updated the README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# task-board
-A task board for managing projects and their deadlines
+# Task-Board
+
+## Description
+This project was made to build familiarity with bootsrap and jquery. It uses local storage to save and load tasks on a task board and uses a bootstrap modal as a form for the user to enter tasks. 
+
+[A link to the website](https://akaufmanfrey.github.io/task-board/)
+![A screenshot of the website](./assets/board_screenshot.PNG)
+
+## Installation
+
+N/A
+
+## Usage
+
+A tool for a user to organize tasks into "To-Do", "In progress", and "Done" columns. The user can click and drag tasks from one column to another to track their completion. It also color codes cards based on whether the due date is near or if they are overdue.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -18,40 +18,40 @@ function readProjectsFromStorage() {
     }
   
 }
-
+// Function for creating a single task card
 function createTaskCard(project) {
   const taskCard = $('<div>');
   taskCard.addClass('card project-card draggable my-3');
   taskCard.attr('data-project-id', project.id);
-  // TODO: Create a new card header element and add the classes `card-header` and `h4`. Also set the text of the card header to the project name.
+  //Create a new card header element and add the classes `card-header` and `h4`. Also set the text of the card header to the project name.
   const cardHeader = $('<div>');
   cardHeader.addClass('card-header h4');
   cardHeader.text(project.name); 
-  // TODO: Create a new card body element and add the class `card-body`.
+  //Create a new card body element
   const cardBody = $('<div>');
   cardBody.addClass('card-body');
-  // TODO: Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project type.
+  //Create a new paragraph element for the project description
   const cardType = $('<p>');
   cardType.addClass('card-text');
   cardType.text(project.type);
-  // TODO: Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project due date.
+  //Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project due date.
   const cardDate = $('<p>');
   cardDate.addClass('card-text');
   cardDate.text(project.dueDate);
-  // TODO: Create a new button element and add the classes `btn`, `btn-danger`, and `delete`. Also set the text of the button to "Delete" and add a `data-project-id` attribute and set it to the project id.
+  //Create a new button element for the delete button
   const cardDeleteBtn = $('<button>');
   cardDeleteBtn.addClass('btn btn-danger delete');
   cardDeleteBtn.text('Delete');
-  cardDeleteBtn.attr('data-project-id', `${project.id}`);
+  cardDeleteBtn.attr('data-project-id', project.id);
 
   cardDeleteBtn.on('click', handleDeleteTask);
 
-  // ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
+  //Sets the card background color based on due date if the task is not done
   if (project.dueDate && project.status !== 'done') {
     const now = dayjs();
     const taskDueDate = dayjs(project.dueDate, 'DD/MM/YYYY');
 
-    // ? If the task is due today, make the card yellow. If it is overdue, make it red.
+    //If the task is due today, make the card yellow. If it is overdue, make it red.
     if (now.isSame(taskDueDate, 'day')) {
       taskCard.addClass('bg-warning text-white');
     } else if (now.isAfter(taskDueDate)) {
@@ -60,19 +60,18 @@ function createTaskCard(project) {
     }
   }
 
-  // TODO: Append the card description, card due date, and card delete button to the card body.
+  //Append the card description, card due date, and card delete button to the card body.
   cardBody.append(cardType, cardDate, cardDeleteBtn);
-  // TODO: Append the card header and card body to the card.
+  //Append the card header and card body to the card.
   taskCard.append(cardHeader, cardBody);
-  // ? Return the card so it can be appended to the correct lane.
   return taskCard;
 }
 
-// Todo: create a function to render the task list and make cards draggable
+// Functino to render all tasks
 function renderTaskList() {
     const projects = readProjectsFromStorage();
 
-    // ? Empty existing project cards out of the lanes
+    //Empty existing project cards out of the lanes
     const todoList = $('#todo-cards');
     todoList.empty();
   
@@ -82,7 +81,7 @@ function renderTaskList() {
     const doneList = $('#done-cards');
     doneList.empty();
   
-    // TODO: Loop through projects and create project cards for each status
+    // Create project cards for each status
     for (let project of projects) {
       const projectCard = createTaskCard(project);
       if (project.status === 'to-do') {
@@ -102,7 +101,8 @@ function renderTaskList() {
       zIndex: 100,
       //Create clone of card which is dragged
       helper: function (e) {
-        //Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card that is draggable and clone that.
+        //Check if the target of the drag event is the card itself or a child element. 
+        //If it is the card itself, clone it, otherwise find the parent card that is draggable and clone that.
         const original = $(e.target).hasClass('ui-draggable')
           ? $(e.target)
           : $(e.target).closest('.ui-draggable');
@@ -114,17 +114,16 @@ function renderTaskList() {
     });
 }
 
-// Todo: create a function to handle adding a new task
+// Event handler for adding a task
 function handleAddTask(event){
   event.preventDefault();
 
-  // TODO: Get the project name, type, and due date from the form
+  //Get the project name, type, and due date from the form
   const projectName = projectNameInputEl.val().trim();
   const projectDate = projectDateInputEl.val();
   const projectType = projectTypeInputEl.val();
-  // ? Create a new project object with the data from the form
+  //Create a new project object with the data from the form
   const newProject = {
-    // ? Here we use a tool called `crypto` to generate a random id for our project. This is a unique identifier that we can use to find the project in the array. `crypto` is a built-in module that we can use in the browser and Nodejs.
     id: crypto.randomUUID(),
     name: projectName,
     type: projectType,
@@ -132,28 +131,28 @@ function handleAddTask(event){
     status: 'to-do',
   };
 
-  // ? Pull the projects from localStorage and push the new project to the array
+  //Pull the projects from localStorage and push the new project to the array
   const projects = readProjectsFromStorage();
   projects.push(newProject);
 
-  // ? Save the updated projects array to localStorage
+  //Save the updated projects array to localStorage
   localStorage.setItem('tasks', JSON.stringify(projects));
 
-  // ? Print project data back to the screen
+  //Print project data back to the screen
   renderTaskList();
 
-  // TODO: Clear the form inputs
+  //Clear the form inputs
   projectDateInputEl.val('');
   projectNameInputEl.val('');
   projectTypeInputEl.val('');
 }
 
-// Todo: create a function to handle deleting a task
+//Handler for delete button press
 function handleDeleteTask(event){
   const projectId = $(this).attr('data-project-id');
   const projects = readProjectsFromStorage();
 
-  // TODO: Loop through the projects array and remove the project with the matching id.
+  //Loop through the projects array and remove the project with the matching id.
   projects.forEach((project, index) => {
     if (project.id === projectId) {
       projects.splice(index, 1);
@@ -161,34 +160,33 @@ function handleDeleteTask(event){
   })
 
   localStorage.setItem('tasks', JSON.stringify(projects));
-
-  // ? Here we use our other function to print projects back to the screen
+  //print remaining projects to the screen
   renderTaskList();
 }
 
-// Todo: create a function to handle dropping a task into a new status lane
+//Handler for dropping a task card
 function handleDrop(event, ui) {
-  // ? Read projects from localStorage
+  //Read projects from localStorage
   const projects = readProjectsFromStorage();
 
-  // ? Get the project id from the event
+  //Get the project id from the event
   const taskId = ui.draggable[0].dataset.projectId;
   console.log(ui.draggable[0]);
-  // ? Get the id of the lane that the card was dropped into
+  //Get the id of the lane that the card was dropped into
   const newStatus = event.target.id;
 
   for (let project of projects) {
-    // ? Find the project card by the `id` and update the project status.
+    //Find the project card by the `id` and update the project status.
     if (project.id === taskId) {
       project.status = newStatus;
     }
   }
-  // ? Save the updated projects array to localStorage (overwritting the previous one) and render the new project data to the screen.
+  //Save the updated projects array to localStorage (overwritting the previous one) and render the new project data to the screen.
   localStorage.setItem('tasks', JSON.stringify(projects));
   renderTaskList();
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+// When the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
   renderTaskList();
   projectFormEl.on('submit', handleAddTask);
